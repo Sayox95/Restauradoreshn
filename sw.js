d7fcc9ebd11d141f5cabf0947ccaea1675e6b0f4
Formularios UTCD <sayoxlive@gmail.com>
2026-04-16 15:57:43 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   HTML/assets → Cache first (carga instantánea)
  *
  * Para forzar actualización en todos los dispositivos:
- *   Cambia CACHE_NAME por ej: 'scada-202604160904'
+ *   Cambia CACHE_NAME por ej: 'scada-202604160907'
  */
 
 const CACHE_NAME    = 'scada-202604161007';
