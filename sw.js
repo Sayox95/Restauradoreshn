be0f830737459fa44f38ab5f3db70d3116288bd0
Formularios UTCD <sayoxlive@gmail.com>
2026-04-16 13:59:37 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
  *   HTML/assets → Cache first (carga instantánea)
  *
  * Para forzar actualización en todos los dispositivos:
- *   Cambia CACHE_NAME por ej: 'scada-202604160900'
+ *   Cambia CACHE_NAME por ej: 'scada-202604160901'
  */
 
 const CACHE_NAME    = 'scada-202604160700';
